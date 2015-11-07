f6a2278ed7457c5a2ae2c6d6425ef3e32042dad6
Teepareep <nicholas.teeple@gmail.com>
2015-11-07 13:53:30 -0800

    Add assignips script

---
+++ assignips.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# shell script to help assign accounts to the proper IP addresses.
+# written by Nick Teeple
+# version .01
+
+for i in $(/scripts/ipusage | awk '{print$1}' | sed '1,2d')
+do
+	#This sed line needs to be adjusted to look for the current ADDR instead of just doing the first line
+    echo "sed -i '1s/.*/ADDR $i/' /etc/wwwacct.conf"
+    for j in $( /scripts/ipusage | cut -d" " -f1,3 | sed "s/,/ /g" | sed "s/]//g" | grep $i | sed 's/\s/\n/g' | sed 1d )
+    do
+           echo "/usr/local/cpanel/bin/setsiteip -u $user `/scripts/whoowns $j` $i"
+    done
+  echo "" #### print the new line ###
+done
